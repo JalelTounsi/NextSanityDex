7d844d604b70edad6594ed2124b834e7c7f5e1b8
Jalel Tounsi <jalel.tounsi@gmail.com>
2022-11-19 14:47:45 +0100

    setup serenity

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    </div>
+  )
+}
+
+
